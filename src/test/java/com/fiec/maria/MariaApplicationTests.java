65f1f6145b7a8880718baea12e94ecd11c627988
unknown <37412@fiecalunos.lan>
2025-03-27 19:25:21 -0300

    marialuiza

---
+++ src/test/java/com/fiec/maria/MariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiec.maria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
